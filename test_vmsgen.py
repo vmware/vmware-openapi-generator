f57f0aa960ff0b4d5dab916141cfc243794e2df1
J.R. Garcia <jrg@vmware.com>
2018-11-05 16:24:46 -0600

    Make SSL verify by default
    This commit makes SSL certificate verification the default and adds an
    option (-k and --insecure) to bypass the verification. The option names
    (both short and long) come from curl.
    
    Signed-off-by: J.R. Garcia <jrg@vmware.com>

--- test_vmsgen.py
+++ test_vmsgen.py
@@ -1,3 +1,4 @@
+import sys
 import vmsgen
 
 def test_tags_generation_from_short_name():
@@ -9,3 +10,17 @@ def test_tags_generation_from_proper_name():
     expected = ['levels_deep']
     actual = vmsgen.tags_from_service_name('more.than.three.levels.deep')
     assert expected == actual
+
+def test_default_ssl_security_option(monkeypatch):
+    with monkeypatch.context() as m:
+         expected = True
+         m.setattr(sys, 'argv', ['a', '-vc', 'b'])
+         _, _, _, actual = vmsgen.get_input_params()
+         assert expected == actual
+
+def test_setting_insecure_ssl_security_option(monkeypatch):
+    with monkeypatch.context() as m:
+         expected = False
+         m.setattr(sys, 'argv', ['a', '-vc', 'b', '-k'])
+         _, _, _, actual = vmsgen.get_input_params()
+         assert expected == actual
