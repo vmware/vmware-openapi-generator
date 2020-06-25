acfa85f1085f08985858b7f4b174485af546accd
Anton Obretenov <obretenova@vmware.com>
2020-06-25 00:37:25 -0700

    Add authentication component builder
    Added authentication component build class, responsible for converting
    info data incoming from the authentication metadata service to
    AuthenticationComponents.
    
    Signed-off-by: Anton Obretenov <obretenova@vmware.com>

--- lib/authentication_metadata_processing.py
+++ lib/authentication_metadata_processing.py
@@ -11,6 +11,53 @@ def get_authentication_dict(auth_component_svc):
                 auth_dict[package_name]
 
 
+def get_operation_scheme_top_level(auth_dict, package_name, service_name, operation_name):
+    pass
+
+
+def get_operation_scheme_package_level(auth_component, service_name, operation_name):
+    pass
+
+
+def get_operation_scheme_service_level(auth_component, operation_name):
+    pass
+
+
+def get_operation_scheme(auth_component):
+    pass
+
+
+class AuthenticationComponentBuilder:
+
+    @staticmethod
+    def build_package_level_component(package_info):
+        package_component = AuthenticationComponent()
+        package_component.add_schemes(AuthenticationComponentBuilder.__extract_schemes_list(package_info))
+        for service_name, service_info in six.iteritems(package_info.services):
+            service_component = AuthenticationComponentBuilder.build_service_level_component(service_info)
+            package_component.add_subcomponent(service_component, service_name)
+        return package_component
+
+    @staticmethod
+    def build_service_level_component(service_info):
+        service_component = AuthenticationComponent()
+        service_component.add_schemes(AuthenticationComponentBuilder.__extract_schemes_list(service_info))
+        for operation_name, operation_info in six.iteritems(service_info.operations):
+            operation_component = AuthenticationComponentBuilder.build_operation_level_component(operation_info)
+            service_component.add_subcomponent(operation_component, operation_name)
+        return service_component
+
+    @staticmethod
+    def build_operation_level_component(operation_info):
+        operation_component = AuthenticationComponent()
+        operation_component.add_schemes(AuthenticationComponentBuilder.__extract_schemes_list(operation_info))
+        return operation_component
+
+    @staticmethod
+    def __extract_schemes_list(auth_component):
+        return [auth_info.scheme for auth_info in auth_component.schemes]
+
+
 class AuthenticationComponent:
 
     def __init__(self):
@@ -26,16 +73,14 @@ class AuthenticationComponent:
     def get_subcomponents(self):
         return self.subcomponents_dict
 
-    def add_authentication_component(self, added_auth_component, auth_component_name):
+    def add_subcomponent(self, added_auth_component, auth_component_name):
         if auth_component_name not in self.subcomponents_dict:
             self.subcomponents_dict[auth_component_name] = added_auth_component
         else:
             existing_component = self.subcomponents_dict[auth_component_name]
             existing_component.add_schemes(added_auth_component.get_schemes())
             for name_added_subcomponent, added_subcomponent in six.iteritems(added_auth_component.get_subcomponents()):
-                existing_component.add_authentication_component(added_subcomponent, name_added_subcomponent)
-
-    #def get_
+                existing_component.add_subcomponent(added_subcomponent, name_added_subcomponent)
 
 
 
--- test_authentication_metadata_processing.py
+++ test_authentication_metadata_processing.py
@@ -1,11 +1,12 @@
 import unittest
 
 from lib.authentication_metadata_processing import AuthenticationComponent
-
+from lib.authentication_metadata_processing import AuthenticationComponentBuilder
+from unittest import mock
 
 class TestAuthenticationComponent(unittest.TestCase):
 
-    def test_authentication_component_operations(self):
+    def test_authentication_component(self):
         package_level_auth_component = AuthenticationComponent()
         package_level_auth_component.add_schemes(["basic_auth"])
 
@@ -14,7 +15,7 @@ class TestAuthenticationComponent(unittest.TestCase):
 
         service_level_auth_component = AuthenticationComponent()
         service_level_auth_component.add_schemes(["token"])
-        package_level_auth_component.add_authentication_component(service_level_auth_component, "my.token.auth.service")
+        package_level_auth_component.add_subcomponent(service_level_auth_component, "my.token.auth.service")
 
         subcomponents_dict = package_level_auth_component.get_subcomponents()
         expected_subcomponent_scheme = {"token"}
@@ -24,9 +25,9 @@ class TestAuthenticationComponent(unittest.TestCase):
         operation_level_auth_component.add_schemes(["saml_token"])
         service_level_auth_component_updated = AuthenticationComponent()
         service_level_auth_component_updated.add_schemes({"token", "oauth2"})
-        service_level_auth_component_updated.add_authentication_component(operation_level_auth_component,
+        service_level_auth_component_updated.add_subcomponent(operation_level_auth_component,
                                                                           "my.token.auth.service.list")
-        package_level_auth_component.add_authentication_component(service_level_auth_component_updated,
+        package_level_auth_component.add_subcomponent(service_level_auth_component_updated,
                                                                   "my.token.auth.service")
 
         expected_subcomponent_scheme = {"token", "oauth2"}
@@ -34,11 +35,11 @@ class TestAuthenticationComponent(unittest.TestCase):
 
         operation_level_auth_component_updated = AuthenticationComponent()
         operation_level_auth_component_updated.add_schemes(["session_id"])
-        service_level_auth_component_updated.add_authentication_component(operation_level_auth_component_updated,
+        service_level_auth_component_updated.add_subcomponent(operation_level_auth_component_updated,
                                                                           "my.token.auth.service.list")
-        service_level_auth_component_updated.add_authentication_component(operation_level_auth_component_updated,
+        service_level_auth_component_updated.add_subcomponent(operation_level_auth_component_updated,
                                                                           "my.token.auth.service.create")
-        package_level_auth_component.add_authentication_component(service_level_auth_component_updated,
+        package_level_auth_component.add_subcomponent(service_level_auth_component_updated,
                                                                   "my.token.auth.service")
 
         subcomponents_dict = package_level_auth_component.get_subcomponents()
@@ -51,6 +52,31 @@ class TestAuthenticationComponent(unittest.TestCase):
                          {"session_id"})
 
 
+    def test_authentication_component_builder(self):
+        session_id_auth_info_mock = mock.Mock()
+        session_id_auth_info_mock.scheme = "session_id"
+        token_auth_info_mock = mock.Mock()
+        token_auth_info_mock.scheme = "token"
+        oauth_auth_info_mock = mock.Mock()
+        oauth_auth_info_mock.scheme = "oauth"
+        operation_info_mock = mock.Mock()
+        operation_info_mock.schemes = [session_id_auth_info_mock, token_auth_info_mock]
+        service_info_mock = mock.Mock()
+        service_info_mock.schemes = [token_auth_info_mock, oauth_auth_info_mock]
+        service_info_mock.operations = {"create": operation_info_mock}
+        package_info_mock = mock.Mock()
+        package_info_mock.services = {"com.vmware.cis.session": service_info_mock}
+        package_info_mock.schemes = [oauth_auth_info_mock, session_id_auth_info_mock]
+
+        package_component = AuthenticationComponentBuilder.build_package_level_component(package_info_mock)
+        expected_package_level_set = {"oauth", "session_id"}
+        self.assertEqual(package_component.get_schemes(), expected_package_level_set)
+        service_component = package_component.get_subcomponents()["com.vmware.cis.session"]
+        expected_schemes = {"oauth", "token"}
+        self.assertEqual(service_component.get_schemes(), expected_schemes)
+        operation_component = service_component.get_subcomponents()["create"]
+        expected_schemes = {"session_id", "token"}
+        self.assertEqual(operation_component.get_schemes(), expected_schemes)
 
 if __name__ == '__main__':
     unittest.main()
