12466b658483c5c4e0c6857bdf15c3d9bddd720b
Delian Mitankin <dmitankin@vmware.com>
2020-12-23 18:19:17 +0200

    [VAPI-4801] Fix deprecation links in the api-explorer
    This change fixes the following three issues:
    1. /api/session is not linked to /rest/com/vmware/cis/session (due to the lack
       of /cis in the new representation)
    2. vmw-task=true (i.e. @TaskOnly) operations are not linked properly
    3. the new token-exchange is not linked to the old one (there is no metadata
       for that link)
    
    It also fixes the filename used in the deprecation path by removing the
    'api_' prefix. This doesn't affect the api-explorer in any way.
    
    Testing done: switched the 3 affected files on a deployment and verified
    that operations are now properly linked.
    
    Signed-off-by: Anton Obretenov <obretenova@vmware.com>

--- lib/rest_endpoint/rest_deprecation_handler.py
+++ lib/rest_endpoint/rest_deprecation_handler.py
@@ -10,21 +10,27 @@ class RestDeprecationHandler:
         self.replacement_dict = replacement_dict
 
     def add_deprecation_information(self, path_obj, package_name, service_name):
-        replacement_path = "<unknown>"
-        path_obj["deprecated"] = True
-
-        # construct file name
-        api_file_name = "api_" + package_name + ".json"
-
+        method = None
+        new_path = None
         # Could be a more intelligent resolution - guessing based on key words?
         operation_map = self.replacement_dict.get(service_name)
         if operation_map is not None and "operationId" in path_obj:
             method_path_tuple = operation_map.get(path_obj["operationId"])
             if method_path_tuple is not None:
-                # get concrete path and format accordingly
-                replacement_path = method_path_tuple[1]
-                replacement_path = replacement_path.replace("/", "~1")
-                replacement_path = api_file_name + "#/paths/~1api" + replacement_path + "/" + method_path_tuple[0]
+                method = method_path_tuple[0]
+                new_path = method_path_tuple[1]
+
+        RestDeprecationHandler.add_deprecation_information_raw(path_obj, package_name, method, new_path)
+
+    @staticmethod
+    def add_deprecation_information_raw(path_obj, package_name, method, new_path):
+        replacement_path = "<unknown>"
+        path_obj["deprecated"] = True
+
+        # construct file name
+        api_file_name = package_name + ".json"
+
+        if method is not None and new_path is not None:
+            replacement_path = api_file_name + "#/paths/~1api" + new_path.replace("/", "~1") + "/" + method
 
         path_obj["x-vmw-deprecated"] = {"replacement": replacement_path}
-
